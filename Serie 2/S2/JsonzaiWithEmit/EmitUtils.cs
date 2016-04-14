2776cec565f426111a2af5bfcb08d8615fc8f7e8
GregoriaCunha007 <40622@alunos.isel.ipl.pt>
2016-04-15 00:53:55 +0100

    Serie 2 uploaded

---
+++ Serie 2/S2/JsonzaiWithEmit/EmitUtils.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection.Emit;
+using System.Reflection;
+
+namespace JsonzaiWithEmit
+{
+    class EmitUtils
+    {
+        public static ModuleBuilder CreateModuleBuilder(AssemblyName aName,out AssemblyBuilder ab)
+        {
+            ab = AppDomain.CurrentDomain.DefineDynamicAssembly(
+                    aName,
+                    AssemblyBuilderAccess.RunAndSave);
+            ModuleBuilder mb = ab.DefineDynamicModule(aName.Name, aName.Name + ".dll");
+            return mb;
+        }
+    }
+}
---
+++ Serie 2/S2/JsonzaiWithEmit/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ISerializerInterface;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace JsonzaiWithEmit
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static void CreateSerializer(object target)
+        {
+            AssemblyBuilder abuilder;
+            AssemblyName name = new AssemblyName("Serializer" + target.GetType().Name);
+            ModuleBuilder mb = EmitUtils.CreateModuleBuilder(name, out abuilder);
+
+            TypeBuilder tb = mb.DefineType(target.GetType().Name  + "Serializer");
+            tb.AddInterfaceImplementation(typeof(ISerializer));
+
+            FieldInfo[] fields = target.GetType().GetFields();
+
+            for( )
+        }
+
+        static void LoadSerializer(object target)
+        {
+           Assembly loadedAsm = Assembly.
+           LoadFrom(@"C:\Users\Pedro\Desktop\1\Isel\6 SEMESTRE\AVE\Greg_Ave_works\Serie 2\S2\JsonzaiWithEmit\bin\Debug\Serializer" + target.GetType().Name + ".dll");
+
+            Type[] types = loadedAsm.GetTypes();
+
+            object obj = Activator.CreateInstance(types[0]);
+
+            MethodInfo[] methodsInDll = obj.GetType().GetMethods();
+
+            foreach (MethodInfo m in methodsInDll)
+            {
+                m.Invoke(obj, new object[] { });
+            }
+        }
+    }
+}
